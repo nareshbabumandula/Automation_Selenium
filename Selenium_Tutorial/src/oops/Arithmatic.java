2926a6447b00432bf7575e94455139ef50bec2ff
Mandula Naresh Babu <naresh2223@gmail.com>
2021-05-04 20:18:46 +0530

    04-May-2021:Naresh: Memory management in Java

--- Selenium_Tutorial/src/oops/Arithmatic.java
+++ Selenium_Tutorial/src/oops/Arithmatic.java
@@ -3,8 +3,10 @@ package oops;
 public class Arithmatic {
 
 	// State/Characteristics/Attributes/Data Members
-	int a=10;
+	int a=10; // Instance variables
 	int b=20;
+	int c;
+	byte d;
 
 	// Behavior or action
 	// addition : Method name
@@ -23,30 +25,46 @@ public class Arithmatic {
 		int c=a*b;
 		System.out.println("Multiplication of a and b is : " + c);
 	}
-	
+
 	void division() {
 		int c=b/a;
 		System.out.println("Division of a and b is : " + c);
 	}
-	
+
 	void modulus() {
 		int c=b%a;
 		System.out.println("Modulus of a and b is : " + c);
 	}
-
+	
+	static void sum() {
+		System.out.println("Executing sum method..!");
+	}
+	
+	// No argument Constructor
+	public Arithmatic() {
+		System.out.println("This is an user defined no argument explicit constructor..!");
+	}
+	
 	public static void main(String[] args) {
-		
+
 		// Create an object reference for the class
 		// Syntax: Classname objRefName = new Constructor();
 		Arithmatic arth = new Arithmatic();
+		Arithmatic arth1 = new Arithmatic();
+		Arithmatic arth2 = arth;
 		arth.addition();
 		arth.subtraction();
 		arth.multiplication();
 		arth.division();
-		arth.modulus();
+		arth.modulus(); // Method call along with the object reference - arth
 		System.out.println(arth.a);
 		System.out.println(arth.b);
-		
+		System.out.println(arth.c);
+		System.out.println(arth.d);
+		sum(); // Method call along without object reference
+		System.out.println(arth.toString());
+		System.out.println(arth1.toString());
+		System.out.println(arth2.toString());
 
 	}
 
---
+++ Selenium_Tutorial/src/oops/ClassA.java
@@ -0,0 +1,9 @@
+package oops;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		System.out.println("This is ClassA main method..!");
+	}
+
+}
