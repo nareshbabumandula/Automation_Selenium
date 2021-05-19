5b920326424d618251fc5f2b6b2fa1430c5f437b
Mandula Naresh Babu <naresh2223@gmail.com>
2021-05-19 21:25:35 +0530

    19-May-2021: Naresh: String methods

---
+++ Selenium_Tutorial/src/basics/StringBufferTest.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class StringBufferTest {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- Selenium_Tutorial/src/basics/StringManipulationTest.java
+++ Selenium_Tutorial/src/basics/StringManipulationTest.java
@@ -11,6 +11,9 @@ public class StringManipulationTest {
 	String sText5 = "       welcome to selenium training                    ";
 	
 	public void stringMethods() {
+		System.out.println(sText1.hashCode());
+		sText1 = "hi";
+		System.out.println(sText1.hashCode());
 		
 		System.out.println(sText1.toUpperCase());
 		System.out.println(sText2.toLowerCase());
@@ -26,11 +29,54 @@ public class StringManipulationTest {
 		System.out.println(sText3.startsWith("hello"));
 		System.out.println(sText4.endsWith("available"));
 		System.out.println(sText1.hashCode());
+		System.out.println(sText4.length());
+		System.out.println(sText4.substring(0, 200));
+		
+		String[] asText  = sText4.split(" ");
+		System.out.println("No of elements in the array asText are : " + asText.length);
+		System.out.println(asText[0]);
+		System.out.println(asText[1]);
+		System.out.println(asText[2]);
+		System.out.println(asText[3]);
+				
+		for (String element : asText) {
+			System.out.println(element);
+		}
+		
+		String result = sText4.join(">", asText); // Join the array element with '>' symbol
+		System.out.println(result);
+		
+		System.out.println(sText1.concat(sText2));
+		System.out.println(sText1.concat(" ").concat(sText2.toLowerCase()));
+		
+		String sText5 = "welcome";
+		System.out.println(sText5.length());
+		
+		for (int i = sText5.length(); i-->0;) {
+			System.out.print(sText5.charAt(i));
+		}
+		System.out.println(" ");
+		
+		System.out.println(sText1.compareTo("hello1"));
+		System.out.println(sText2.isEmpty());
+		System.out.println(sText2.isBlank());
+		
+		char[] res = sText1.toCharArray();
+		for (char c : res) {
+			System.out.println(c);
+		}
+		String r="";
+		for (int i=res.length-1;i>=0;i--) {
+			r+=res[i];
+		}
+		System.out.println(r);
+	
 	}
 
 	public static void main(String[] args) {
 		StringManipulationTest sm = new StringManipulationTest();
 		sm.stringMethods();
+
 
 	}
 
